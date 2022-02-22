3181e774fdbc338cee25ac5f2ddc83fab3070c28
DakhovnikMaksim <dahovnik@gmail.com>
2022-02-22 17:43:02 +0200

    Clear code solution

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,50 +1,11 @@
 ﻿using System.Windows;
 
-namespace DigitalTimer;
+namespace TimerWPF;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
-    private Timer timer;
-    private int count;
-
     public MainWindow()
     {
         InitializeComponent();
-        timer = new Timer(this);
-        BtnStop.IsEnabled = false;
-        BtnInterval.IsEnabled = false;
-    }
-
-    private void BtnStart_Click(object sender, RoutedEventArgs e)
-    {
-        timer.Start();
-        BtnStart.IsEnabled = false;
-        BtnStop.IsEnabled = true;
-        BtnInterval.IsEnabled = true;
-
-    }
-
-    private void BtnStop_Click(object sender, RoutedEventArgs e)
-    {
-        timer.Stop();
-        BtnStart.IsEnabled = true;
-        BtnStop.IsEnabled = false;
-        BtnInterval.IsEnabled = false;
-    }
-
-    private void BtnInterval_Click(object sender, RoutedEventArgs e)
-    {
-        count++;
-        LabelInterval.Content += $"{count}. {timer.GetInterval()}\n";
-
-    }
-
-    private void BtnClear_Click(object sender, RoutedEventArgs e)
-    {
-        count = 0;
-        LabelInterval.Content = "";
     }
 }
--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,171 +1,165 @@
-﻿using Timer;
-
+﻿
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Windows.Controls.Primitives;
-using System.Windows.Input;
 using System.Windows.Threading;
 
-namespace Timer
+namespace TimerWPF;
+
+public class MainWindowViewModel : INotifyPropertyChanged
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    #region Fields
+    private DateTime startTime;
+
+    private TimeSpan milliSecond;
+
+    private double ms;
+
+    private string timerContent;
+    #endregion
+
+    #region Properties
+    private DispatcherTimer DispatcherTimer { get; }
+
+    public string TimerContent
     {
-        #region Fields
-        private DateTime startTime;
-
-        private TimeSpan milliSecond;
-
-        private double ms;
-
-        private string timerContent;
-        #endregion
-
-        #region Properties
-        private DispatcherTimer DispatcherTimer { get; }
-
-        public string TimerContent
+        get { return timerContent; }
+        set
         {
-            get { return timerContent; }
-            set
-            {
-                timerContent = value;
-                OnPropertyChanged("TimerContent");
-            }
+            timerContent = value;
+            OnPropertyChanged("TimerContent");
         }
+    }
 
-        private int Count { get; set; }
+    private int Count { get; set; }
 
-        private double Difference { get; set; }
+    private double Difference { get; set; }
 
-        public ObservableCollection<string> IntervalContent { get; }
-        #endregion
+    public ObservableCollection<string> IntervalContent { get; }
+    #endregion
 
-        #region BtnIsEnabled
-        private bool btnStopIsEnabled;
-        public bool BtnStopIsEnabled
+    #region BtnIsEnabled
+    private bool btnStopIsEnabled;
+    public bool BtnStopIsEnabled
+    {
+        get { return btnStopIsEnabled; }
+        set
         {
-            get { return btnStopIsEnabled; }
-            set
-            {
-                btnStopIsEnabled = value;
-                OnPropertyChanged("BtnStopIsEnabled");
-            }
+            btnStopIsEnabled = value;
+            OnPropertyChanged("BtnStopIsEnabled");
         }
+    }
 
-        private bool btnStartIsEnabled;
-        public bool BtnStartIsEnabled
+    private bool btnStartIsEnabled;
+    public bool BtnStartIsEnabled
+    {
+        get { return btnStartIsEnabled; }
+        set
         {
-            get { return btnStartIsEnabled; }
-            set
-            {
-                btnStartIsEnabled = value;
-                OnPropertyChanged("BtnStartIsEnabled");
-            }
+            btnStartIsEnabled = value;
+            OnPropertyChanged("BtnStartIsEnabled");
         }
+    }
 
-        private bool btnIntervalIsEnabled;
-        public bool BtnIntervalIsEnabled
+    private bool btnIntervalIsEnabled;
+    public bool BtnIntervalIsEnabled
+    {
+        get { return btnIntervalIsEnabled; }
+        set
         {
-            get { return btnIntervalIsEnabled; }
-            set
-            {
-                btnIntervalIsEnabled = value;
-                OnPropertyChanged("BtnIntervalIsEnabled");
-            }
+            btnIntervalIsEnabled = value;
+            OnPropertyChanged("BtnIntervalIsEnabled");
         }
+    }
 
-        private bool btnClearIsEnabled;
-        public bool BtnClearIsEnabled
+    private bool btnClearIsEnabled;
+    public bool BtnClearIsEnabled
+    {
+        get { return btnClearIsEnabled; }
+        set
         {
-            get { return btnClearIsEnabled; }
-            set
-            {
-                btnClearIsEnabled = value;
-                OnPropertyChanged("BtnClearIsEnabled");
-            }
+            btnClearIsEnabled = value;
+            OnPropertyChanged("BtnClearIsEnabled");
         }
-        #endregion
+    }
+    #endregion
 
-        #region INotifyProp
-        public event PropertyChangedEventHandler PropertyChanged;
+    #region INotifyProp
+    public event PropertyChangedEventHandler PropertyChanged;
 
-        private void OnPropertyChanged([CallerMemberName] string prop = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
-        }
-        #endregion
+    private void OnPropertyChanged([CallerMemberName] string prop = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+    #endregion
 
-        #region Commands
-        public Command ClearCommand { get; }
-        public Command GetIntervalCommand { get; }
-        public Command StartCommand { get; }
-        public Command StopCommand { get; }
-        #endregion
+    #region Commands
+    public Command ClearCommand { get; }
+    public Command GetIntervalCommand { get; }
+    public Command StartCommand { get; }
+    public Command StopCommand { get; }
+    #endregion
 
-        #region Ctor
-        public MainWindowViewModel()
-        {
-            ClearCommand = new Command(ClearInterval);
-            GetIntervalCommand = new Command(GetInterval);
-            StartCommand = new Command(Start);
-            StopCommand = new Command(Stop);
+    #region Ctor
+    public MainWindowViewModel()
+    {
+        ClearCommand = new Command(ClearInterval);
+        GetIntervalCommand = new Command(GetInterval);
+        StartCommand = new Command(Start);
+        StopCommand = new Command(Stop);
 
-            IntervalContent = new ObservableCollection<string>();
-            TimerContent = "00:00:00:000";
-            DispatcherTimer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 0, 0, 1) };
-            DispatcherTimer.Tick += Timer_Tick;
+        IntervalContent = new ObservableCollection<string>();
+        TimerContent = "00:00:00:000";
+        DispatcherTimer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 0, 0, 1) };
+        DispatcherTimer.Tick += Timer_Tick;
 
-            BtnStartIsEnabled = true;
-        }
-        #endregion
+        BtnStartIsEnabled = true;
+    }
+    #endregion
 
-        private void ClearInterval(object obj)
-        {
-            Count = 0;
-            IntervalContent.Clear();
-            BtnClearIsEnabled = false;
-        }
+    private void ClearInterval(object obj)
+    {
+        Count = 0;
+        IntervalContent.Clear();
+        BtnClearIsEnabled = false;
+    }
 
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            milliSecond = DateTime.Now - startTime;
-            ms = milliSecond.TotalMilliseconds;
-            TimerContent = ms.ToString("00:00:00:000", CultureInfo.InvariantCulture);
-        }
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        milliSecond = DateTime.Now - startTime;
+        ms = milliSecond.TotalMilliseconds;
+        TimerContent = ms.ToString("00:00:00:000", CultureInfo.InvariantCulture);
+    }
 
-        public void Start(object obj)
-        {
-            startTime = DateTime.Now;
-            DispatcherTimer.Start();
-            BtnStopIsEnabled = true;
-            BtnIntervalIsEnabled = true;
-            BtnStartIsEnabled = false;
-        }
+    public void Start(object obj)
+    {
+        startTime = DateTime.Now;
+        DispatcherTimer.Start();
+        BtnStopIsEnabled = true;
+        BtnIntervalIsEnabled = true;
+        BtnStartIsEnabled = false;
+    }
 
-        public void Stop(object obj)
-        {
-            DispatcherTimer.Stop();
-            BtnStartIsEnabled = true;
-            BtnStopIsEnabled = false;
-            BtnIntervalIsEnabled = false;
-        }
+    public void Stop(object obj)
+    {
+        DispatcherTimer.Stop();
+        BtnStartIsEnabled = true;
+        BtnStopIsEnabled = false;
+        BtnIntervalIsEnabled = false;
+    }
 
-        public void GetInterval(object obj)
-        {
-            Count++;
-            BtnClearIsEnabled = true;
+    public void GetInterval(object obj)
+    {
+        Count++;
+        BtnClearIsEnabled = true;
 
-            var res = Count == 1
-                ? "00:00:000"
-                : (ms - Difference).ToString("00:00:000", CultureInfo.InvariantCulture);
+        var res = Count == 1
+            ? "00:00:000"
+            : (ms - Difference).ToString("00:00:000", CultureInfo.InvariantCulture);
 
-            IntervalContent.Add($"{Count}. {TimerContent}  (+{res})");
-            Difference = ms;
-        }
+        IntervalContent.Add($"{Count}. {TimerContent}  (+{res})");
+        Difference = ms;
     }
 }
