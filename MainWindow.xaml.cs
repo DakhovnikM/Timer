328d4b6c8baf4cef0f9b1d164aa801e5a49d8366
DakhovnikMaksim <dahovnik@gmail.com>
2022-02-22 17:21:52 +0200

    Up to net 6.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,64 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace DigitalTimer
+namespace DigitalTimer;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private Timer timer;
+    private int count;
+
+    public MainWindow()
     {
-        private Timer timer;
-        private int count;
+        InitializeComponent();
+        timer = new Timer(this);
+        BtnStop.IsEnabled = false;
+        BtnInterval.IsEnabled = false;
+    }
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            timer = new Timer(this);
-            BtnStop.IsEnabled = false;
-            BtnInterval.IsEnabled = false;
-        }
+    private void BtnStart_Click(object sender, RoutedEventArgs e)
+    {
+        timer.Start();
+        BtnStart.IsEnabled = false;
+        BtnStop.IsEnabled = true;
+        BtnInterval.IsEnabled = true;
 
-        private void BtnStart_Click(object sender, RoutedEventArgs e)
-        {
-            timer.Start();
-            BtnStart.IsEnabled = false;
-            BtnStop.IsEnabled = true;
-            BtnInterval.IsEnabled = true;
+    }
 
-        }
+    private void BtnStop_Click(object sender, RoutedEventArgs e)
+    {
+        timer.Stop();
+        BtnStart.IsEnabled = true;
+        BtnStop.IsEnabled = false;
+        BtnInterval.IsEnabled = false;
+    }
 
-        private void BtnStop_Click(object sender, RoutedEventArgs e)
-        {
-            timer.Stop();
-            BtnStart.IsEnabled = true;
-            BtnStop.IsEnabled = false;
-            BtnInterval.IsEnabled = false;
-        }
+    private void BtnInterval_Click(object sender, RoutedEventArgs e)
+    {
+        count++;
+        LabelInterval.Content += $"{count}. {timer.GetInterval()}\n";
 
-        private void BtnInterval_Click(object sender, RoutedEventArgs e)
-        {
-            count++;
-            LabelInterval.Content += $"{count}. {timer.GetInterval()}\n";
+    }
 
-        }
-
-        private void BtnClear_Click(object sender, RoutedEventArgs e)
-        {
-            count = 0;
-            LabelInterval.Content = "";
-        }
+    private void BtnClear_Click(object sender, RoutedEventArgs e)
+    {
+        count = 0;
+        LabelInterval.Content = "";
     }
 }
--- Timer.cs
+++ Timer.cs
@@ -1,51 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Controls.Primitives;
 using System.Windows.Threading;
 
-namespace DigitalTimer
+namespace DigitalTimer;
 
+public class Timer
 {
-    public class Timer
+    public MainWindow mainWindow;
+    private readonly DispatcherTimer dispatcherTimer;
+    private DateTime startTime;
+
+    private TimeSpan milliSecond;
+
+    private string ms;
+
+    public Timer(MainWindow window)
     {
-        public MainWindow mainWindow;
-        private readonly DispatcherTimer dispatcherTimer;
-        private DateTime startTime;
+        mainWindow = window;
 
-        private TimeSpan milliSecond;
-
-        private string ms;
-        
-        public Timer(MainWindow window)
-        {
-            mainWindow = window;
-
-            dispatcherTimer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 0, 0, 1) };
-            dispatcherTimer.Tick += Timer_TickSub;
-        }
-
-        private void Timer_TickSub(object sender, EventArgs e)
-        {
-            milliSecond = DateTime.Now - startTime;
-            ms = milliSecond.TotalMilliseconds.ToString("00:00:00:000");
-            mainWindow.LabelTimer.Content = ms;
-        }
-
-        public void Start()
-        {
-            startTime = DateTime.Now;
-            dispatcherTimer.Start();
-        }
-        public void Stop()
-        {
-            dispatcherTimer.Stop();
-        }
-
-        public string GetInterval()
-        {
-            return ms;
-        }
-
+        dispatcherTimer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 0, 0, 1) };
+        dispatcherTimer.Tick += Timer_TickSub;
     }
+
+    private void Timer_TickSub(object sender, EventArgs e)
+    {
+        milliSecond = DateTime.Now - startTime;
+        ms = milliSecond.TotalMilliseconds.ToString("00:00:00:000");
+        mainWindow.LabelTimer.Content = ms;
+    }
+
+    public void Start()
+    {
+        startTime = DateTime.Now;
+        dispatcherTimer.Start();
+    }
+    public void Stop()
+    {
+        dispatcherTimer.Stop();
+    }
+
+    public string GetInterval()
+    {
+        return ms;
+    }
+
 }
